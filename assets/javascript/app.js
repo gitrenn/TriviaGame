0b2c9fa47d4970f0d1501758df9e2813642ec662
Ren <rjingsiu@gmail.com>
2017-11-01 23:28:23 -0700

    created count down function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-    
+    var count = 30; 
+    var intervalId;
+
+
+    $(".start-game").click(function(){
+        countDown();
+        showQuestions();
+    });
+
+    function countDown(){
+        if(intervalId){
+            return;
+        }
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement(){
+        count--;
+
+        $(".count-down").html("<h2>" + "Counting Down! You have: " + count + "s left!" + "</h2>");
+
+        if(count === 0){
+            stop();
+        }
+    }
+
+    function stop(){
+        clearInterval(intervalId);
+        intervalId = null;
+    }
 })
