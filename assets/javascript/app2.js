e6f739a55bf8ee1ef3242975cf6c2dd8f9c619b1
Ren <rjingsiu@gmail.com>
2017-11-03 09:20:01 -0700

    added a hard-coded version

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -2,92 +2,102 @@ $(document).ready(function () {
     var count = 30;
     var intervalId;
     var renderDiv = false;
+    var selected1, selected2, selected3, selected4, selected5;
     var questionArray = [
         {
             question: "1. What is the name of the rogue agency that takes extreme measures to defend the federation?",
             options: ["Shadow Proclamation", "Section 31", "Starfleet Academy", "Borg Collective"],
-            answer: "option2"
+            answer: "Section 31"
         },
         {
             question: "2. What is Captain Janeway's favorite drink?",
-            options: ["Coffee, Black", "Raktajino", "Slug-o-Cola", "Tuber root"],
-            answer: "option1"
+            options: ["Black Coffee", "Raktajino", "Slug-o-Cola", "Tuber root"],
+            answer: "Black Coffee"
         },
         {
-            question: "3. Whatsgfsdgfsfd gg  the rogue agency that takes extreme measures to defend the federation?",
-            options: ["Irish Beer", "Shadow Proclamation", "Starfleet Academy", "Section 31"],
-            answer: "Section 31"
+            question: "3. Who told this to Kirk 'Captain, not in front of the Klingons'?",
+            options: ["Uhura", "Spock"],
+            answer: "Spock"
         },
         {
-            question: "4. Whatsgfsdgfsfd gg  the rogue agency that takes extreme measures to defend the federation?",
-            options: ["Irish Beer", "Shadow Proclamation", "Starfleet Academy", "Section 31"],
-            answer: "Section 31"
+            question: "4. Who became the ruler of Terran Empire in the Mirror Darkly?",
+            options: ["Archer", "Sato"],
+            answer: "Sato"
         },
         {
-            question: "5. Whatsgfsdgfsfd gg  the rogue agency that takes extreme measures to defend the federation?",
-            options: ["Irish Beer", "Shadow Proclamation", "Starfleet Academy", "Section 31"],
-            answer: "Section 31"
+            question: "5. Who did Quark temporarily married to?",
+            options: ["Dax", "Grilka"],
+            answer: "Grilka"
         }
     ]
-    // hide div when loaded
-    $(".q-div").hide();
 
-    var q_len = questionArray.length;
-    
-        for (var i = 0; i < q_len; i++) {
-            var o_len = questionArray[i].options.length;
-            // create a card div to hold one question
-            var card_div = $("<div>").attr("class", "card");
-            // create a card header
-            var title_div = $("<div>").attr("class", "card-header");
-            // add each question to the card header
-            title_div.text(questionArray[i].question);
-            // place card header inside the card div
-            card_div.append(title_div);
-            // loop through the options array to render the options  
-            //for(var j = 0; j <o_len; j++){
-            // create a card body div to hold an option
-            var option_div = $("<div>").attr("class", "card-body");
-            //create an option 
-            var form_div = $("<div>").attr("class", "form-check");
-            var label_div = $("<label>").addClass("form-check-label");
-            var input_div = $("<input>").addClass("form-check-input");
-            // add type, value and text
-            input_div.attr("type", "radio");
-            input_div.attr("name", "inlineRadioOptions");
-            input_div.attr("value", "option1");
-            label_div.append(input_div)
-            label_div.append(questionArray[i].options[0])
-            option_div.append(form_div.append(label_div));
-    
-            // add a second option
-            var form_div = $("<div>").attr("class", "form-check");
-            var label_div = $("<label>").addClass("form-check-label");
-            var input_div = $("<input>").addClass("form-check-input");
-            // add type, value and text
-            input_div.attr("type", "radio");
-            input_div.attr("name", "inlineRadioOptions");
-            input_div.attr("value", "option2");
-            label_div.append(input_div)
-            label_div.append(questionArray[i].options[1])
-            option_div.append(form_div.append(label_div));
     
     
-            console.log(option_div);
-            //console.log(option);
-            //}
-            card_div.append(option_div);
-            //         var newDiv = $("<div>").attr("id", "title");  
-            //         // append to the new div
-            //         newDiv.text(questionArray[i].question);
-            //         //$(".q-title").append(newDiv);
-            //    } 
-            function createForm() {
-    
-            }
-            $(".q-body").append(card_div);
-        }
-   
+    // hide div when loaded
+    hideDiv();
+
+    // add questions 
+    $(".q1").text(questionArray[0].question);
+        $(".q1-o1").append(questionArray[0].options[0]);
+        $(".q1-o2").append(questionArray[0].options[1]);
+    $(".q2").text(questionArray[1].question);
+        $(".q2-o1").append(questionArray[1].options[0]);
+        $(".q2-o2").append(questionArray[1].options[1]);
+    $(".q3").text(questionArray[2].question);
+        $(".q3-o1").append(questionArray[2].options[0]);
+        $(".q3-o2").append(questionArray[2].options[1]);
+    $(".q4").text(questionArray[3].question);
+        $(".q4-o1").append(questionArray[3].options[0]);
+        $(".q4-o2").append(questionArray[3].options[1]);
+    $(".q5").text(questionArray[4].question);
+        $(".q5-o1").append(questionArray[4].options[0]);
+        $(".q5-o2").append(questionArray[4].options[1]);
+    // store user selections     
+    if($(".q1-o1").click(function(){
+        selected1=($(".q1-o1").text().trim());
+        console.log(selected1);
+    }))
+    if($(".q1-o2").click(function(){
+        selected1=($(".q1-o2").text().trim());
+        console.log(selected1);
+    }))
+
+    if($(".q2-o1").click(function(){
+        selected2=($(".q2-o1").text().trim());
+        console.log(selected2);
+    }))
+    if($(".q2-o2").click(function(){
+        selected2=($(".q2-o2").text().trim());
+        console.log(selected2);
+    }))
+
+    if($(".q3-o1").click(function(){
+        selected3=($(".q3-o1").text().trim());
+        console.log(selected3);
+    }))
+    if($(".q3-o2").click(function(){
+        selected3=($(".q3-o2").text().trim());
+        console.log(selected3);
+    }))
+
+    if($(".q4-o1").click(function(){
+        selected4=($(".q4-o1").text().trim());
+        console.log(selected4);
+    }))
+    if($(".q4-o2").click(function(){
+        selected4=($(".q4-o2").text().trim());
+        console.log(selected4);
+    }))
+
+    if($(".q5-o1").click(function(){
+        selected5=($(".q5-o1").text().trim());
+        console.log(selected5);
+    }))
+    if($(".q5-o2").click(function(){
+        selected5=($(".q5-o2").text().trim());
+        console.log(selected5);
+    }))
+
 
     $(".start-game").click(function () {
         countDown();
@@ -104,6 +114,7 @@ $(document).ready(function () {
 
     function renderQuestions(){
         $(".q-div").show();
+
     }
 
     function decrement() {
@@ -115,7 +126,10 @@ $(document).ready(function () {
         if (count === 0) {
             stop();
             hideDiv();
-            //checkAnswer();
+            checkAnswer();
+            count = 30;
+            disableGame();
+
         }
     }
 
@@ -130,10 +144,19 @@ $(document).ready(function () {
     }
 
     function checkAnswer(){
-        var selected = $(this).val();
-        console.log(selected);
-        //if(selected === questionArray)
+        if(selected1 !== questionArray[0].answer
+        || selected2 !== questionArray[1].answer
+        || selected3 !== questionArray[2].answer
+        || selected4 !== questionArray[3].answer
+        || selected5 !== questionArray[4].answer){
+            alert("You failed to deploy armor, you will be assimilated by the Borg!");
+            $(".count-down").html("You failed, everyone on the ship is dead");
+        }
+        else {alert("You successfully deployed the armor, you saved everyone on the ship!");}
     }
     
-    $(document).on("click",".form-check-input", checkAnswer);
+    function disableGame(){
+        $(".start-game").hide();
+    }
+    //$(document).on("click",".form-check-input", checkAnswer);
 })
